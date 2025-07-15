5e3eee63177f5147fecb00f0ad3b5caa059aa95e
sarthakchy <choudharysarthak.6@gmail.com>
2025-07-16 01:54:03 +0530

    clean and load crm_sales_details

--- scripts/silver/silver_load.sql
+++ scripts/silver/silver_load.sql
@@ -63,3 +63,45 @@ SELECT
 	CAST(LEAD(prd_start_dt) OVER(PARTITION BY prd_key ORDER BY prd_start_dt)-1 as DATE) as prd_end_dt
 FROM bronze.crm_prd_info;
 GO
+
+-- LOAD Customer Sales Data (crm_sales_details)
+INSERT INTO silver.crm_sales_details(
+	sls_ord_num,
+	sls_prd_key,
+	sls_cust_id,
+	sls_order_dt,
+	sls_ship_dt,
+	sls_due_dt,
+	sls_sales,
+	sls_quantity,
+	sls_price
+)
+SELECT
+	sls_ord_num,
+	sls_prd_key,
+	sls_cust_id,
+	CASE
+		WHEN sls_order_dt<=0 OR len(sls_order_dt)!=8 THEN NULL
+		ELSE CAST(CAST(sls_order_dt as VARCHAR) as DATE)
+	END as sls_order_dt,
+	CASE
+		WHEN sls_ship_dt<=0 OR len(sls_ship_dt)!=8 THEN NULL
+		ELSE CAST(CAST(sls_ship_dt as VARCHAR) as DATE)
+	END as sls_ship_dt,
+	CASE
+		WHEN sls_due_dt<=0 OR len(sls_due_dt)!=8 THEN NULL
+		ELSE CAST(CAST(sls_due_dt as VARCHAR) as DATE)
+	END as sls_due_dt,
+	CASE
+		WHEN sls_sales <=0 OR sls_sales IS NULL or sls_sales != sls_quantity * ABS(sls_price) 
+			THEN sls_quantity * ABS(sls_price)
+		ELSE sls_sales
+	END as sls_sales,
+	sls_quantity,
+	CASE
+		WHEN sls_price <=0 OR sls_price IS NULL
+			THEN sls_sales/NULLIF(sls_quantity,0)
+		ELSE sls_price
+	END as sls_price
+FROM bronze.crm_sales_details
+GO
